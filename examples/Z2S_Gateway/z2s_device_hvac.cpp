6cf0ea2f60b48949208ab3bf3f6a7fedddbac31b
lsroka76 <95999074+lsroka76@users.noreply.github.com>
2025-05-01 17:33:33 +0100

    Update z2s_device_hvac.cpp

--- examples/Z2S_Gateway/z2s_device_hvac.cpp
+++ examples/Z2S_Gateway/z2s_device_hvac.cpp
@@ -165,7 +165,7 @@ void msgZ2SDeviceHvac(int16_t channel_number_slot, uint8_t msg_id, int32_t msg_v
       if (Supla_Z2S_HvacBase->isWeeklyScheduleEnabled()) {
         if (abs(Supla_Z2S_HvacBase->getTemperatureSetpointHeat() - msg_value) > 50) {
           TWeeklyScheduleProgram program = Supla_Z2S_HvacBase->getProgramById(Supla_Z2S_HvacBase->getCurrentProgramId());
-          Supla_Z2S_HvacBase->setProgram(Supla_Z2S_HvacBase->getCurrentProgramId(), program.Mode, msg_value);
+          Supla_Z2S_HvacBase->setProgram(Supla_Z2S_HvacBase->getCurrentProgramId(), program.Mode, msg_value, program.SetpointTemperatureCool, false);
           Supla_Z2S_TRVInterface->setTRVTemperatureSetpoint(msg_value); 
         }
       }
